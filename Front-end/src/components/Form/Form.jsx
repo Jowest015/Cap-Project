7448e9750136341245f02cda577b62cb588b1921
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-09-11 11:30:42 -0500

    added icons to buttons

--- Front-end/src/components/Form/Form.jsx
+++ Front-end/src/components/Form/Form.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import useStyles from './styles';
 
@@ -18,49 +20,62 @@ const Form = () => {
 
   }
 
-const classes = useStyles();
+  const classes = useStyles();
 
 
   return (
     <Paper className={classes.paper}>
       <form className={classes.form} onSubmit={handleSubmit}>
-        <Typography>Submit Form</Typography>
+        <Typography variant="h6">Submit Form</Typography>
         <TextField 
           name="author"
-          variant="standard"
+          variant="outlined"
           label="author"
           value={postInfo.author}
           onChange={(e) => setPostInfo({ author: e.target.value })}
           />
           <TextField 
           name="title"
-          variant="standard"
+          variant="outlined"
           label="title"
           value={postInfo.title}
           onChange={(e) => setPostInfo({ title: e.target.value })}
           />
           <TextField 
           name="message"
-          variant="standard"
+          variant="outlined"
           label="message"
           value={postInfo.message}
           onChange={(e) => setPostInfo({ message: e.target.value })}
           />
           <TextField 
           name="tags"
-          variant="standard"
+          variant="outlined"
           label="tags"
           value={postInfo.tags}
           onChange={(e) => setPostInfo({ tags: e.target.value })}
           />
           <TextField 
           name="selectedFile"
-          variant="standard"
+          variant="outlined"
           label="selectedFile"
           value={postInfo.selectedFile}
           onChange={(e) => setPostInfo({ selectedFile: e.target.value })}
           />
-          <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" fullWidth>Submit</Button>
+          <Button 
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          startIcon={<CloudUploadIcon />}
+          size="large" 
+          fullWidth>Submit</Button>
+          <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          startIcon={<DeleteIcon />}
+          size="large"
+          fullWidth>Delete</Button>
       </form>
     </Paper>
   )
