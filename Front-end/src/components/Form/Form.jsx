268bd23c8164f4f80c2da3a3f23dbf82a1a8d597
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-09-11 09:49:07 -0500

    filling out input form

--- Front-end/src/components/Form/Form.jsx
+++ Front-end/src/components/Form/Form.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
-import { TextField, Button, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
-//Following the template add textfield, buttons and props with typography
+
+
+//Following the template add textfield, buttons and props with typography   * note to self need file transfer ability for pics
 
 const Form = () => {
+  const [postInfo, setPostInfo] = useState({author:'', title:'', message:'', tags:'', selectedFile:''})
+
+  const handleSubmit = () => {
+
+  }
+
+
+
+
+
+
   return (
-    <h1>FORM</h1>
+    <Paper>
+      <form on onSubmit={handleSubmit}>
+        <Typography>Showcase</Typography>
+        <TextField 
+          name="author"
+          variant="standard"
+          label="author"
+          value={postInfo.author}
+          onChange={(e) => setPostInfo({ author: e.target.value })}
+          />
+          <TextField 
+          name="title"
+          variant="standard"
+          label="title"
+          value={postInfo.title}
+          onChange={(e) => setPostInfo({ title: e.target.value })}
+          />
+          <TextField 
+          name="message"
+          variant="standard"
+          label="message"
+          value={postInfo.message}
+          onChange={(e) => setPostInfo({ message: e.target.value })}
+          />
+          <TextField 
+          name="tags"
+          variant="standard"
+          label="tags"
+          value={postInfo.tags}
+          onChange={(e) => setPostInfo({ tags: e.target.value })}
+          />
+          <TextField 
+          name="selectedFile"
+          variant="standard"
+          label="selectedFile"
+          value={postInfo.selectedFile}
+          onChange={(e) => setPostInfo({ selectedFile: e.target.value })}
+          />
+          <Button></Button>
+      </form>
+    </Paper>
   )
 }
 
