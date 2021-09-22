7911b1cee13719581dd2ef90980872c10005590c
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-09-21 20:19:44 -0500

    deploying heroku

--- Back-end/index.js
+++ Back-end/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://goog:goog123@cluster0.qkffh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
